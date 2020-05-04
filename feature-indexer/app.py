be72ab5b852d8e6591281a9d6a5162ea6545026d
helissonnas <helisson@copin.ufcg.edu.br>
2020-05-04 14:12:42 -0300

    wip

--- feature-indexer/app.py
+++ feature-indexer/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+import data_access
+from flask import Flask, make_response, jsonify
+import json
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/service')
+def get_services():
+    all_services = data_access.find_all_services()
+    return make_response(jsonify(services=all_services), 200)
+
+
+@app.route('/feature')
+def get_features():
+    all_features = data_access.find_all_features()
+    return make_response(jsonify(features=all_features), 200)
+
 
 if __name__ == '__main__':
     app.run()
