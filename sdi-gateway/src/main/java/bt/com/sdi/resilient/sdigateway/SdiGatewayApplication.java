209a2ce7ca7e6dd7d5bf72808ec03814f1f61e79
Helisson Nascimento <helissonnas@MacBook-Air-de-Helisson.local>
2021-06-03 17:03:43 -0300

    gateway done

--- sdi-gateway/src/main/java/bt/com/sdi/resilient/sdigateway/SdiGatewayApplication.java
+++ sdi-gateway/src/main/java/bt/com/sdi/resilient/sdigateway/SdiGatewayApplication.java
@@ -2,14 +2,11 @@ package bt.com.sdi.resilient.sdigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("bt.com.sdi.resilient.sdigateway")
 public class SdiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SdiGatewayApplication.class, args);
 	}
-
 }
--- sdi-gateway/src/test/java/bt/com/sdi/resilient/sdigateway/SdiGatewayApplicationTests.java
+++ sdi-gateway/src/test/java/bt/com/sdi/resilient/sdigateway/SdiGatewayApplicationTests.java
@@ -6,8 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SdiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
 }
